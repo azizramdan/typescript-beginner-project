f2accd760367ac49f5b2630c3782106fe0b459c8
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2022-02-28 13:23:51 +0700

    array type

--- 1-my-first-code/app.ts
+++ 1-my-first-code/app.ts
@@ -1,9 +1,15 @@
 const person: {
   name: string;
   age: number;
+  hobbies: string[];
 } = {
   name: 'Aziz Ramdan Kurniawan',
   age: 24,
+  hobbies: ['Eat', 'Cooking']
 }
 
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
