00b2f474ecc98be69ad865a1086309c4e27f556a
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2022-02-28 15:07:45 +0700

    unknown data type

--- 1-my-first-code/app.ts
+++ 1-my-first-code/app.ts
@@ -1,31 +1,12 @@
-function add(n1: number, n2: number): number {
-  return n1 + n2
+let userInput: unknown
+let userName: string
+
+userInput = 5
+userInput = 'Aziz'
+
+userInput = userName
+// userName = userInput // error
+
+if (typeof userInput === 'string') {
+  userName = userInput // success
 }
-
-function printResultVoid(num: number): void {
-  console.log('Result: ' + num);
-}
-
-function printResultUndefined(num: number): undefined {
-  console.log('Result: ' + num);
-  return
-}
-
-function addAndHandle(n1: number, n2: number, callback: (num: number) => void) {
-  const result = n1 + n2
-  callback(result)
-}
-
-printResultVoid(add(1, 7))
-
-// let combineValues: Function
-let combineValues: (a: number, b:number) => number
-
-combineValues = add
-// combineValues = printResultVoid
-
-console.log(combineValues(7, 1));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-})
