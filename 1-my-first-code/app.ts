c2f1df54bf6e20548e9951e7611db9303abf8ba5
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2022-02-28 13:27:09 +0700

    tuple type

--- 1-my-first-code/app.ts
+++ 1-my-first-code/app.ts
@@ -2,10 +2,12 @@ const person: {
   name: string;
   age: number;
   hobbies: string[];
+  role: [number, string];
 } = {
   name: 'Aziz Ramdan Kurniawan',
   age: 24,
-  hobbies: ['Eat', 'Cooking']
+  hobbies: ['Eat', 'Cooking'],
+  role: [1, 'Author'],
 }
 
 console.log(person.name);
